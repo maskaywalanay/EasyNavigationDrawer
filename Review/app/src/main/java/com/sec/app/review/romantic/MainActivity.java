c3d75ca81dbe0cc54a7d15de0682f28559d951b1
maskaywalanay <mwalanay@gmail.com>
2019-11-04 07:11:59 +0700

    Added string

--- Review/app/src/main/java/com/sec/app/review/romantic/MainActivity.java
+++ Review/app/src/main/java/com/sec/app/review/romantic/MainActivity.java
@@ -2,13 +2,37 @@ package com.sec.app.review.romantic;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
+import android.view.View.*;
+import android.view.*;
+import android.content.*;
 
 public class MainActivity extends Activity 
+
 {
+	ImageView imageView;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		imageView = (ImageView)findViewById(R.id.d_exit);
+		imageView.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View p1)
+				{
+					Intent inten = new Intent();
+					inten.setClassName("eu.chainfire.supersu","eu.chainfire.supersu.MainActivity");
+					startActivity(inten);
+					Toast.makeText(getApplicationContext(),R.string.toast_clicked_a,Toast.LENGTH_SHORT).show();
+				}
+				
+			
+		});
+		
     }
 }
